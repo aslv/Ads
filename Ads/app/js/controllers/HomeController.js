d879f8c23d71df9a9972866b57567a48b9faf955
aslv <aslv@abv.bg>
2015-01-07 01:07:09 +0200

    home page in progress

--- Ads/app/js/controllers/HomeController.js
+++ Ads/app/js/controllers/HomeController.js
@@ -1,3 +1,10 @@
 adsApp.controller('HomeController', ['$scope', '$rootScope', 'ads', 'notify', function($scope, $rootScope, ads, notify) {
-	// TODO: 
+	ads.getAds(
+	null,
+	function success(data) {
+		$scope.ads = data;
+	},
+	function error(error) {
+		notify.error('An error occured while loading ads.', error);
+	});
 }]);
--- Ads/app/js/services/ads.js
+++ Ads/app/js/services/ads.js
@@ -1,5 +1,9 @@
 adsApp.factory('ads', ['$resource', 'baseUrl', function ($resource, baseUrl) {
+	var adsResource = $resource(baseUrl + 'ads', null, { 'getAll': { method: 'GET' } });
+	function getAllAds(params, success, error) {
+		return adsResource.getAll(params, success, error);
+	}
 	return {
-		// TODO:
+		getAds: getAllAds
 	};
 }]);
