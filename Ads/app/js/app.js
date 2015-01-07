553dbe33dcd319895488368f70fea1f4c6af5c2d
aslv <aslv@abv.bg>
2015-01-07 14:42:39 +0200

    left and right sidebars created

--- Ads/app/js/app.js
+++ Ads/app/js/app.js
@@ -1,4 +1,4 @@
-var adsApp = angular.module('adsApp', ['ngRoute', 'ngResource']);
+var adsApp = angular.module('adsApp', ['ngRoute', 'ngResource', 'ui.bootstrap.pagination']);
 
 adsApp.constant('baseUrl', 'http://softuni-ads.azurewebsites.net/api/');
 
