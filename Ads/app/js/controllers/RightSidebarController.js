0d54093a867bb2ba1f5a733bdef7530da60fac36
aslv <aslv@abv.bg>
2015-01-09 01:07:13 +0200

    categories and towns loaders added

--- Ads/app/js/controllers/RightSidebarController.js
+++ Ads/app/js/controllers/RightSidebarController.js
@@ -1,4 +1,7 @@
 adsApp.controller('RightSidebarController', ['$scope', '$rootScope', 'categories', 'towns', 'notify', function($scope, $rootScope, categories, towns, notify) {
+	$scope.categoriesLoading = true;
+	$scope.townsLoading = true;
+
 	categories.getCategories()
 		.$promise
 		.then(
@@ -7,6 +10,11 @@ adsApp.controller('RightSidebarController', ['$scope', '$rootScope', 'categories
 			},
 			function error(error) {
 				notify.error('An error occured while loading categories.', error);
+			}
+		)
+		.finally(
+			function categoriesLoaded() {
+				$scope.categoriesLoading = false;
 			}
 		);
 	$scope.categoryClicked = function (clickedCategoryId) {
@@ -22,6 +30,11 @@ adsApp.controller('RightSidebarController', ['$scope', '$rootScope', 'categories
 			},
 			function error(error) {
 				notify.error('An error occured while loading towns.', error);
+			}
+		)
+		.finally(
+			function townsLoaded() {
+				$scope.townsLoading = false;
 			}
 		);
 	$scope.townClicked = function (clickedTownId) {
