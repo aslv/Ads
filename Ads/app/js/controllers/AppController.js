e3cbcb6365d312f6b8abbd5c02d55a369fff18fd
aslv <aslv@abv.bg>
2015-01-09 16:44:44 +0200

    status filter in progress

--- Ads/app/js/controllers/AppController.js
+++ Ads/app/js/controllers/AppController.js
@@ -1,5 +1,6 @@
 adsApp.controller('AppController', ['$scope', '$location', 'user', 'notify', function($scope, $location, user, notify) {
 	$scope.user = user;
+	$scope.location = $location;
 
 	$scope.logout = function () {
 		user.logout();
